f46f4b86ee44467ee0a0b2022cbd0225315a5915
Aaron Cleveland <toscleveland@gmail.com>
2019-12-17 17:47:17 -0600

    Finished Afternoon Project

--- Stars/Model Controller/StarController.swift
+++ Stars/Model Controller/StarController.swift
@@ -14,6 +14,11 @@ class StarControllers {
     
     var stars: [Star] = []
     
+    // this is the starcontrollers init. we dont need to pass in anything but we can use it more as a viewdidload for the starcontroller
+    init() {
+        loadFromPersistentStore()
+    }
+    
     // Read
         // Any time the app wants to access it's stars, they'll live in the StarController
     
@@ -23,7 +28,10 @@ class StarControllers {
     @discardableResult func createStar(with name: String, distance: Double) -> Star {
         // at the least, init a star and return it.
         let star = Star(name: name, distance: distance)
+        
         stars.append(star)
+        saveToPersistentStore()
+        
         return star
     }
     
@@ -35,10 +43,51 @@ class StarControllers {
         let fileManager = FileManager.default
         
         guard let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        let starsURL = documentsDir.appendingPathComponent("stars.plist")
+        return starsURL
     }
     
     // saving function
     
+    func saveToPersistentStore() {
+            
+        // make sure the fileURL exists, otherwise we cant save the stars there.
+        guard let fileURL = persistentFileURL else { return }
+        
+        do {
+            
+            let encoder = PropertyListEncoder()
+            
+            let starsData = try encoder.encode(stars)
+            
+            try starsData.write(to: fileURL)
+        } catch {
+            print("Error Saving stars: \(error)")
+        }
+    }
+    
     // loading function
     
+    func loadFromPersistentStore() {
+        
+        guard let fileURL = persistentFileURL else { return }
+        
+        do {
+            let starsData = try Data(contentsOf: fileURL)
+            
+            let decoder = PropertyListDecoder()
+            
+            let starsArray = try decoder.decode([Star].self, from: starsData)
+            
+            self.stars = starsArray
+            
+        } catch {
+            
+            print("Error loading stars from plist: \(error)")
+            
+        }
+        
+        
+    }
 }
